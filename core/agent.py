51ac726a394b7912e59b886934d5b76dcdee996c
decipher-code <vishsangule@gmail.com>
2026-06-16 02:08:44 +0530

    Update agent.py

--- core/agent.py
+++ core/agent.py
@@ -7,7 +7,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import networkx as nx
 import chromadb
 from typing import TypedDict, Annotated, List, Union
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage, ToolMessage
 from langgraph.graph import StateGraph, END
